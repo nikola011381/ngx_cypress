0b3d89691bc741791a3dc00dacf69ff33ed4f71f
Nikola <37514211+nikola011381@users.noreply.github.com>
2022-07-31 14:20:43 +0200

    Third test commit

--- cypress/e2e/Stepper_Third.cy.ts
+++ cypress/e2e/Stepper_Third.cy.ts
@@ -13,34 +13,33 @@ describe('Stepper Forma 3', () => {
         .find('button:contains("next") ').first().click()
         cy.get('[class="col-md-12 col-lg-6 col-xxxl-6"] [orientation="vertical"]')
         cy.contains('Step content #2').should('be.visible')
+
         cy.get('[class="col-md-12 col-lg-6 col-xxxl-6"] [orientation="vertical"]')
         .find('button:contains("next") ').first().click()
         cy.get('[class="col-md-12 col-lg-6 col-xxxl-6"] [orientation="vertical"]')
         cy.contains('Step content #3').should('be.visible')
+
         cy.get('[class="col-md-12 col-lg-6 col-xxxl-6"] [orientation="vertical"]')
         .find('button:contains("next") ').first().click()
         cy.get('[class="col-md-12 col-lg-6 col-xxxl-6"] [orientation="vertical"]')
         cy.contains('Step content #4').should('be.visible')
         cy.get('[class="col-md-12 col-lg-6 col-xxxl-6"] [orientation="vertical"]')
         .find('button').contains("next").should('be.not.enabled')
+
         cy.get('[class="col-md-12 col-lg-6 col-xxxl-6"] [orientation="vertical"]')
         .find('button:contains("prev") ').first().click()
-
         cy.get('[class="col-md-12 col-lg-6 col-xxxl-6"] [orientation="vertical"]')
         .find('button').contains("next").first().should('be.enabled')
         cy.get('[class="col-md-12 col-lg-6 col-xxxl-6"] [orientation="vertical"]')
         cy.contains('Step content #3').should('be.visible')
+
         cy.get('[class="col-md-12 col-lg-6 col-xxxl-6"] [orientation="vertical"]')
         .find('button:contains("prev") ').first().click()
-
- 
         cy.get('[class="col-md-12 col-lg-6 col-xxxl-6"] [orientation="vertical"]')
         cy.contains('Step content #2').should('be.visible')
+
         cy.get('[class="col-md-12 col-lg-6 col-xxxl-6"] [orientation="vertical"]')
         .find('button:contains("prev") ').first().click()
- 
-
-
         cy.get('[class="col-md-12 col-lg-6 col-xxxl-6"] [orientation="vertical"]')
          .contains('Step content #1').should('be.visible')
         cy.get('[class="col-md-12 col-lg-6 col-xxxl-6"] [orientation="vertical"]')
