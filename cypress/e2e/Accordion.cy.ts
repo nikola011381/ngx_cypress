df7685c7404e1630cbad3f1c0b9cd7f7f03d8ca2
Nikola <37514211+nikola011381@users.noreply.github.com>
2022-07-31 14:44:01 +0200

    Accordion commit

---
+++ cypress/e2e/Accordion.cy.ts
@@ -0,0 +1,55 @@
+describe('First test Accordion', () => {
+    beforeEach(() => {
+      cy.visit('http://localhost:4200/')
+      cy.get('.sidebar-toggle').click()
+      cy.get('[class="menu-title"]').contains("Layout").click()
+      cy.get('[class="menu-title"]').contains("Accordion").click() 
+      cy.get('.sidebar-toggle').click()
+  
+     })
+ 
+
+    it('Open close open close toggle button ', () => {
+   
+        cy.get('button').contains("Toggle First Item").click()
+        cy.contains('A nebula is an interstellar cloud of dust').should('be.visible')
+        cy.get('button').contains("Toggle First Item").click()
+        cy.contains('A nebula is an interstellar cloud of dust').should('be.not.visible')
+        cy.get('button').contains("Toggle First Item").click()
+        cy.contains('A nebula is an interstellar cloud of dust').should('be.visible')
+        cy.get('button').contains("Toggle First Item").click()
+        cy.contains('A nebula is an interstellar cloud of dust').should('be.not.visible')
+    })
+    
+
+    it('Click on Product Details ', () => {
+   
+     cy.contains('Product Details').click()
+     cy.contains('A nebula is an interstellar cloud of dust').should('be.visible')
+     cy.contains('Product Details').click()
+     cy.contains('A nebula is an interstellar cloud of dust').should('be.not.visible')
+     cy.contains('Product Details').click()
+     cy.contains('A nebula is an interstellar cloud of dust').should('be.visible')
+     cy.contains('Product Details').click()
+     cy.contains('A nebula is an interstellar cloud of dust').should('be.not.visible')
+    })
+
+    it('Combination toggle button and Product Details ', () => {
+   
+        cy.contains('Product Details').click()
+        cy.contains('A nebula is an interstellar cloud of dust').should('be.visible')
+        cy.get('button').contains("Toggle First Item").click()
+        cy.contains('A nebula is an interstellar cloud of dust').should('be.not.visible')
+        cy.contains('Product Details').click()
+        cy.contains('A nebula is an interstellar cloud of dust').should('be.visible')
+        cy.get('button').contains("Toggle First Item").click()
+        cy.contains('A nebula is an interstellar cloud of dust').should('be.not.visible')
+        cy.get('button').contains("Toggle First Item").click()
+        cy.contains('A nebula is an interstellar cloud of dust').should('be.visible')
+        cy.contains('Product Details').click()
+        cy.contains('A nebula is an interstellar cloud of dust').should('be.not.visible')
+
+
+       })
+
+  })    
